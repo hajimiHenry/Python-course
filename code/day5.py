073dcd77994c57eab94d3ce77580c2407ced9668
hajimiHenry <spaner2022@outlook.com>
2026-01-30 22:37:38 +0800

    写了好几个题，写的最开心的是CRAPS赌博游戏 一开始是玩家一直赢，然后改正了变成庄家开始赢一点点了，改到最后才发现原来庄家还这么能赢

--- code/day5.py
+++ code/day5.py
@@ -1,2 +1,14 @@
 #这部分只需要学一下match case这一块
 
+status_code=int(input('相应状态码'))
+match status_code:
+    case 400:description = 'bad request'
+    case 401:description = 'unauthorized'
+    case 403:description = 'forbidden'
+    case 404: description = 'Not Found'
+    case 405: description = 'Method Not Allowed'
+    case 418: description = 'I am a teapot'
+    case 429: description = 'Too many requests'
+    case _: description = 'Unknown Status Code'
+print('状态码描述',description)
+
